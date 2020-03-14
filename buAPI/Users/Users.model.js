3b20b76fbf978242298eb7218bf6846fe89f9fea
odion100 <odionedwards@gmail.com>
2020-03-14 05:57:13 -0400

    created User model

---
+++ buAPI/Users/Users.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+const required = true;
+const unique = true;
+
+module.exports = model(
+  "Users",
+  Schema({
+    _id: Schema.Types.ObjectId,
+    first_name: { type: String, required },
+    last_name: { type: String, required },
+    age: String,
+    gender: String,
+    profile_image: String,
+    banner_image: String,
+    email: { type: String, required, unique },
+    password: { type: String, required },
+    created_date: { type: Date, require },
+    main_zipcode: String,
+    tag: String
+  })
+);
