97bba86863713a9cfb6681b95717870cc2f0b40e
odion100 <odionedwards@gmail.com>
2020-03-30 11:04:46 -0400

    removed console.logs

--- buAPI/Users/Users.js
+++ buAPI/Users/Users.js
@@ -7,7 +7,6 @@ Service.ServerModule("Users", function() {
 
   Users.get = ({ id, email, password, status }, cb) => {
     const queries = [];
-    console.log(id, isValidObjectId(id));
     if (email && password) queries.push({ email, password });
     if (id && isValidObjectId(id)) queries.push({ _id: id });
 
@@ -19,38 +18,34 @@ Service.ServerModule("Users", function() {
     else queries.push({ account_status: status || "Active" });
 
     usersModel
-      .findOne({ $and: queries })
+      .findOne({ $and: queries }, { password: 0 })
       .then(user => {
-        if (user) cb({ user, status: 200 });
-        else cb(null, { message: "Users resource not found", status: 404 });
+        if (user) cb(null, { user, status: 200 });
+        else cb({ message: "Users resource not found", status: 404 });
       })
       .catch(error => cb(error));
   };
 
   Users.add = (data, cb) => {
-    console.log("Request recieved");
     const user = new usersModel({ _id: Types.ObjectId(), ...data });
-    console.log(user);
     user
       .save()
-      .then(newUser => {
-        console.log("saved succesfully-----<<>----");
-        cb(null, { newUser, status: 200, message: "New user created successfully." });
-      })
-      .catch(error => {
-        console.log("a faild attempted womp womp");
-        cb({ error, status: 400, message: "Failed to create new user" });
-      });
+      .then(newUser =>
+        cb(null, { newUser, status: 200, message: "New user created successfully." })
+      )
+      .catch(error => cb({ error, status: 400, message: "Failed to create new user" }));
   };
 
-  Users.updateFields = ({ id, updatedFields }, cb) => {
+  Users.update = ({ id, fields }, cb) => {
+    if (!id || !fields)
+      return cb({ status: 404, message: "Invalid options:Expecting: id, fields" });
     usersModel
-      .findByIdAndUpdate(id, { $set: updatedFields }, { new: true })
+      .findByIdAndUpdate(id, { $set: fields }, { new: true, useFindAndModify: true })
       .then(updatedUser => cb(null, { updatedUser, status: 200 }))
       .catch(error => cb({ error }));
   };
 
-  Users.setAccoutnStatus = async ({ id, status }, cb) => {
+  Users.setAccountStatus = async ({ id, status }, cb) => {
     if (!id || !isValidObjectId(id)) return cb({ message: "Invaild id type:", status: 400 });
 
     try {
