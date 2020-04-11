a9deec18a0edf490265f0da3bc29c57f43fdfdb2
odion100 <odionedwards@gmail.com>
2020-04-11 08:31:37 -0400

    now emitting new_invite event

--- buAPI/Invites/Invites.js
+++ buAPI/Invites/Invites.js
@@ -2,16 +2,17 @@ const { Service } = require("sht-tasks");
 const invitesModel = require("./Invites.model");
 const { Types } = require("mongoose");
 const moment = require("moment");
-Service.ServerModule("Invites", function() {
+Service.ServerModule("Invites", function () {
   const Invites = this;
 
   Invites.add = (data, cb) => {
     new invitesModel({ _id: Types.ObjectId(), ...data })
       .save()
-      .then(newInvite =>
-        cb(null, { newInvite, status: 200, message: "New invite created successfully." })
-      )
-      .catch(error => cb({ error, status: 400, message: "Failed to create new invite" }));
+      .then((newInvite) => {
+        cb(null, { newInvite, status: 200, message: "New invite created successfully." });
+        Invites.emit(`new_invite:${newInvite.target}`, newInvite);
+      })
+      .catch((error) => cb({ error, status: 400, message: "Failed to create new invite" }));
   };
 
   Invites.get = ({ id, source, target }, cb) => {
@@ -23,13 +24,13 @@ Service.ServerModule("Invites", function() {
     if (queries.length === 0)
       return cb({
         message: "Invalid request options. Expecting id or source and/or target",
-        status: 400
+        status: 400,
       });
 
     invitesModel
       .find({ $and: queries }, { password: 0 })
-      .then(invites => cb(null, { invites, status: 200 }))
-      .catch(error => cb(error));
+      .then((invites) => cb(null, { invites, status: 200 }))
+      .catch((error) => cb(error));
   };
 
   Invites.sendResponse = ({ id, message = "", status }, cb) => {
@@ -39,7 +40,7 @@ Service.ServerModule("Invites", function() {
       return cb("invalid options: status must be 'accepted' or 'rejected'");
     invitesModel
       .findOne({ _id: id })
-      .then(invite => {
+      .then((invite) => {
         if (!invite) return cb({ status: 404, message: "invite not found" });
         if (invite.status !== "sent")
           return cb({ status: 403, message: "invite response already recieved", invite });
@@ -49,48 +50,48 @@ Service.ServerModule("Invites", function() {
         invite.response_date = moment().toJSON();
         invite
           .save()
-          .then(updatedInvite => cb(null, { updatedInvite, status: 200 }))
-          .catch(error => cb(error));
+          .then((updatedInvite) => cb(null, { updatedInvite, status: 200 }))
+          .catch((error) => cb(error));
       })
-      .catch(error => cb(error));
+      .catch((error) => cb(error));
   };
 
   Invites.cancel = ({ id }, cb) => {
     if (!id) return cb({ status: 404, message: "invalid options: 'id' is required" });
     invitesModel
       .findOne({ _id: id })
-      .then(invite => {
+      .then((invite) => {
         if (!invite) return cb({ status: 404, message: "invite not found" });
         if (invite.status !== "sent")
           return cb({
             status: 403,
             message: "invite response already recieved, or canceled",
-            invite
+            invite,
           });
 
         invite.status = "canceled";
         invite
           .save()
-          .then(updatedInvite => cb(null, { updatedInvite, status: 200 }))
-          .catch(error => cb(error));
+          .then((updatedInvite) => cb(null, { updatedInvite, status: 200 }))
+          .catch((error) => cb(error));
       })
-      .catch(error => cb(error));
+      .catch((error) => cb(error));
   };
 
   Invites.markAsViewed = ({ id }, cb) => {
     if (!id) return cb({ status: 404, message: "invalid options: 'id' is required" });
     invitesModel
       .findOne({ _id: id })
-      .then(invite => {
+      .then((invite) => {
         if (!invite) return cb({ status: 404, message: "invite not found" });
 
         invite.viewed_date = moment().toJSON();
         invite
           .save()
-          .then(updatedInvite => cb(null, { updatedInvite, status: 200 }))
-          .catch(error => cb(error));
+          .then((updatedInvite) => cb(null, { updatedInvite, status: 200 }))
+          .catch((error) => cb(error));
       })
-      .catch(error => cb(error));
+      .catch((error) => cb(error));
   };
 
   Invites.resend = ({ id, message }, cb) => {
@@ -98,7 +99,7 @@ Service.ServerModule("Invites", function() {
     if (!id) return cb({ status: 404, message: "invalid options: 'id' is required" });
     invitesModel
       .findOne({ _id: id })
-      .then(invite => {
+      .then((invite) => {
         if (!invite) return cb({ status: 404, message: "invite not found" });
 
         invite.viewed_date = null;
@@ -107,9 +108,9 @@ Service.ServerModule("Invites", function() {
 
         invite
           .save()
-          .then(updatedInvite => cb(null, { updatedInvite, status: 200 }))
-          .catch(error => cb(error));
+          .then((updatedInvite) => cb(null, { updatedInvite, status: 200 }))
+          .catch((error) => cb(error));
       })
-      .catch(error => cb(error));
+      .catch((error) => cb(error));
   };
 });
