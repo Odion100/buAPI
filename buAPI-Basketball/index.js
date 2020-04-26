0e73b3e4b76ca27aeb7521d8ba11957ebd16f30d
odion100 <odionedwards@gmail.com>
2020-04-26 07:42:54 -0400

    moved mongoose connection to app.config file

--- buAPI-Basketball/index.js
+++ buAPI-Basketball/index.js
@@ -1,23 +1,13 @@
-require("dotenv").config();
-const route = "bu/api/basketball";
-const port = 4100;
-const useREST = true;
-const { App, Service } = require("sht-tasks");
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(
-    `mongodb+srv://Odion:${process.env.MONGODB_PASSWORD}@cluster0-8s7lw.mongodb.net/test?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-  )
-  .then((data) => console.log("mongodb connected:-->"))
-  .catch((err) => console.log("mongodb connection failed:-->", err));
-
 require("./Users/Users.api");
 require("./Tournaments/Tournaments.api");
 require("./Teams/Teams.api");
 require("./Games/Games.api");
 require("./Callouts/Callouts.api");
+
+const { App } = require("sht-tasks");
+const route = "bu/api/basketball";
+const port = 4100;
+const useREST = true;
 
 App.startService({ route, port, useREST })
 
--- buAPI-Networking/index.js
+++ buAPI-Networking/index.js
@@ -1,17 +1,8 @@
-require("dotenv").config();
+const { App } = require("sht-tasks");
+
 const route = "bu/api/networking";
 const port = 4000;
 const useREST = true;
-const { App } = require("sht-tasks");
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(
-    `mongodb+srv://Odion:${process.env.MONGODB_PASSWORD}@cluster0-8s7lw.mongodb.net/test?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-  )
-  .then((data) => console.log("mongodb connected:-->"))
-  .catch((err) => console.log("mongodb connection failed:-->", err));
 
 require("./Invites/Invites.api");
 //require("./EventFeed/EventFeed.api");
